4374af502ce2d32f485af5612c315dc23ccc4681
Jerry Hwang <hyeoncheol.hwang3@t-mobile.com>
2021-09-11 17:59:38 -0700

    update

--- k8s_examples/simpleweb/index.js
+++ k8s_examples/simpleweb/index.js
@@ -1,7 +1,29 @@
 // This line must come before importing any instrumented module.
+// const tracer = require('dd-trace').init({
+//   logInjection: true
+// });
+
 const tracer = require('dd-trace').init({
   logInjection: true
 });
+
+const formats = require('dd-trace/ext/formats');
+
+class Logger {
+    log(level, message) {
+        const span = tracer.scope().active();
+        const time = new Date().toISOString();
+        const record = { time, level, message };
+
+        if (span) {
+            tracer.inject(span.context(), formats.LOG, record);
+        }
+
+        console.log(JSON.stringify(record));
+    }
+}
+
+module.exports = Logger;
 
 const express = require('express');
 
