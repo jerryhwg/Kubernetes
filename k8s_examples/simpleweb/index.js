1324e20d034c186e1299be82ab787662c0f6da9c
Jerry Hwang <hyeoncheol.hwang3@t-mobile.com>
2021-09-11 20:00:13 -0700

    update

--- k8s_examples/simpleweb/index.js
+++ k8s_examples/simpleweb/index.js
@@ -1,7 +1,5 @@
-// This line must come before importing any instrumented module.
-// const tracer = require('dd-trace').init({
-//   logInjection: true
-// });
+//This line must come before importing any instrumented module.
+// const tracer = require('dd-trace').init();
 
 const tracer = require('dd-trace').init({
   logInjection: true
@@ -36,3 +34,34 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
+
+// const span = tracer.scope().active()
+
+// // add a foo:bar tag
+// span.setTag('foo', 'bar')
+
+// // add a user_id:5 tag
+// span.setTag('user_id', 5)
+
+// // add a obj.first:foo and obj.second:bar tags
+// span.setTag('obj', { first: 'foo', second: 'bar' })
+
+// // add a foo:bar and baz:qux tags
+// span.addTags({
+//   foo: 'bar',
+//   baz: 'qux'
+// })
+
+// app.get('/make-sandwich', (req, res) => {
+//   const sandwich = tracer.trace('sandwich.make', () => {
+//     const ingredients = tracer.trace('get_ingredients', () => {
+//       return getIngredients()
+//     })
+
+//     return tracer.trace('assemble_sandwich', () => {
+//       assembleSandwich(ingredients)
+//     })
+//   })
+
+//   res.end(sandwich)
+// })
